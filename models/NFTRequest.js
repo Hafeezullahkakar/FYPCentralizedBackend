55893fdaa615564796fa8c03afe26743eb6859d3
Hafeezullah kakar <hafeezullahkakar@gmail.com>
2023-03-20 02:44:16 +0500

    added

---
+++ models/NFTRequest.js
@@ -0,0 +1,81 @@
+const mongoose = require("mongoose");
+
+const nftRequestSchema = new mongoose.Schema(
+  {
+    metadata: {
+      propertyName: {
+        type: String,
+        required: true,
+      },
+      propertyType: {
+        type: String,
+        required: true,
+      },
+      propertyAddress: {
+        type: String,
+        required: true,
+      },
+      propertyLocation: {
+        type: String,
+        required: true,
+      },
+      propertyOwner: {
+        type: String,
+        required: true,
+      },
+      propertyStatus: {
+        type: String,
+        required: true,
+      },
+      isApproved: {
+        type: Boolean,
+        required: true,
+        default: false,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      hash: {
+        type: String,
+        required: true
+      },
+      images: [
+        {
+          type: String,
+        },
+      ],
+    },
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "rejected", "signed"],
+      default: "pending",
+    },
+    assignedAgent: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Agent",
+    },
+    tokenId: {
+      type: Number,
+    },
+    call: {
+      callLink: {
+        type: String,
+        default: "",
+      },
+      callTime: {
+        type: String,
+        default: "",
+      },
+      callDate: {
+        type: String,
+        default: "",
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+const NFTRequest = mongoose.model("NFTRequest", nftRequestSchema);
+
+module.exports = NFTRequest;
